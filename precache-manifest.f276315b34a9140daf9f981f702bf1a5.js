a8aadcfb68313668528cfce0dd3d90c944828ad0
Carlos Lira <carloshenriquen.lira@gmail.com>
2018-11-01 18:30:09 +0000

    Updates

---
+++ precache-manifest.f276315b34a9140daf9f981f702bf1a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc047347b17f7df7ff288d78c8c281cf",
+    "url": "/cv/static/media/placeholder.fc047347.png"
+  },
+  {
+    "revision": "084ac45aee39707f976e",
+    "url": "/cv/static/js/runtime~main.084ac45a.js"
+  },
+  {
+    "revision": "1236d6c086cbe461e4a7",
+    "url": "/cv/static/js/main.1236d6c0.chunk.js"
+  },
+  {
+    "revision": "49e5afdd75bdf85132e3",
+    "url": "/cv/static/js/1.49e5afdd.chunk.js"
+  },
+  {
+    "revision": "1236d6c086cbe461e4a7",
+    "url": "/cv/static/css/main.a44306f3.chunk.css"
+  },
+  {
+    "revision": "49e5afdd75bdf85132e3",
+    "url": "/cv/static/css/1.9c1f6524.chunk.css"
+  },
+  {
+    "revision": "e011973c94c2b4ad251e0ca49757dfb2",
+    "url": "/cv/index.html"
+  }
+];
